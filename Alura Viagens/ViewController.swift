d3e874a199990315d12aa85e13c3247225c317fd
fvendrasco <felipe@vendrasco.com.br>
2020-12-29 15:15:38 -0300

    Initial Commit

---
+++ Alura Viagens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alura Viagens
+//
+//  Created by Felipe Augusto Vendrasco on 29/12/20.
+//  Copyright © 2020 Felipe Augusto Vendrasco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

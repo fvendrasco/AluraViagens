ab9fad95a7693742c367bdfb64495676d25a4d3c
fvendrasco <felipe@vendrasco.com.br>
2020-12-29 21:53:03 -0300

    layout do projeto Alura Viagem

---
+++ Alura Viagens/Home/Controller/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Alura Viagens
+//
+//  Created by Felipe Augusto Vendrasco on 29/12/20.
+//  Copyright © 2020 Felipe Augusto Vendrasco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    @IBOutlet weak var tabelaViagens: UITableView!
+    @IBOutlet weak var viewHoteis: UIView!
+    @IBOutlet weak var viewPacotes: UIView!
+    
+    let listaViagens:Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabelaViagens.dataSource = self
+        self.viewPacotes.layer.cornerRadius = 10
+        self.viewHoteis.layer.cornerRadius = 10
+        self.tabelaViagens.delegate = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaViagens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        let viagemAtual = listaViagens[indexPath.row]
+        
+        cell.labelTitulo.text = viagemAtual.titulo
+        cell.labelPreco.text = "R$ \(viagemAtual.preco)"
+        cell.labelQuantidade.text = "\(viagemAtual.quantidadeDeDias) dias"
+        cell.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
+        
+        cell.imagemViagem.layer.cornerRadius = 10
+        cell.imagemViagem.layer.masksToBounds = true
+        
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone ? 175 : 260
+    }
+}
---
+++ Alura Viagens/Home/View/TableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.swift
+//  Alura Viagens
+//
+//  Created by Felipe Augusto Vendrasco on 29/12/20.
+//  Copyright © 2020 Felipe Augusto Vendrasco. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelQuantidade: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+    @IBOutlet weak var imagemViagem: UIImageView!
+}
